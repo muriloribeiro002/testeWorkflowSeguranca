88d3afda66e92120ca2d9070713023e860ecc37c
muriloribeiro002 <muriloribeiro002@gmail.com>
2024-08-21 13:55:43 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,12 @@
+a = int(input('Digite um numero'))
+s = str(input('Digite o sinal'))
+b = int(input('Digite um numero'))
+
+if (s =='+'):
+  print(a+b)
+elif (s == '-'):
+  print(a-b)
+elif (s == '*'):
+  print(a*b)
+else:
+  print(a/b)
